deb531bb09be645b7c4cef7ef1c14ab24200365f
sb0321 <woltaryksb1@gmail.com>
2021-04-14 10:29:21 +0900

    @MappingSuperclass 연습

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,8 +1,6 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Book;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -21,12 +19,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            Book book = new Book();
-            book.setName("JPA");
-            book.setAuthor("김영한");
-
-            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
