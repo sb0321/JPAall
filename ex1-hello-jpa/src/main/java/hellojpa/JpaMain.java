cec321d9c0d179f6bc231979c0087ace81b4706f
sb0321 <woltaryksb1@gmail.com>
2021-04-14 13:37:59 +0900

    CASCADE 를 이용한 영속성 전이

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -18,43 +19,14 @@ public class JpaMain {
 
         try {
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            em.persist(member1);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team = new Team();
-            team.setName("teamA");
-            member1.setTeam(team);
-            em.persist(team);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            em.flush();
-            em.clear();
-
-            Member m = em.find(Member.class, member1.getId());
-
-            System.out.println("m = " + m.getTeam().getClass());
-
-            System.out.println("=================");
-            m.getTeam().getName(); // 초기화
-            System.out.println("=================");
-
-
-//            Member findMember = em.find(Member.class, member1.getId());
-//            System.out.println("reference = " + findMember.getClass()); // Member
-
-////            Member findMember = em.find(Member.class, member.getId());
-//
-//            Member findMember = em.getReference(Member.class, member.getId());
-//
-////            실제 객체가 없는 상태
-//            System.out.println("before findMember = " + findMember.getClass());
-//            System.out.println("findMember.username = " + findMember.getUsername());
-//
-////            프록시가 객체를 받아옴
-//            System.out.println("before findMember = " + findMember.getClass());
-//            System.out.println("findMember.username = " + findMember.getUsername());
-
-
+            em.persist(parent);
 
             tx.commit();
         } catch (Exception e) {
