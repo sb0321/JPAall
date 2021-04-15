8e075564063206802146b3056f7b6db91344464e
sb0321 <woltaryksb1@gmail.com>
2021-04-15 14:11:47 +0900

    엔티티 직접 사용 쿼리

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -44,25 +44,17 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-//            String query = "select t from Team t join fetch t.members m";
-            String query = "select t from Team t";
+//            String query = "select m from Member m where m.id = :memberId";
+            String query = "select m from Member m where m.team = :team";
 
-
-            List<Team> result = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
+            List<Member> members = em.createQuery(query, Member.class)
+                    .setParameter("team", teamA)
                     .getResultList();
 
-            System.out.println("result = " + result.size());
-
-            for (Team team : result) {
-                System.out.println("team = " + team.getName() + " |members" + team.getMembers().size());
-                for (Member member : team.getMembers()) {
-                    System.out.println("-> member = " + member);
-                }
+            for (Member member : members) {
+                System.out.println("member = " + member);
             }
 
-            
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
