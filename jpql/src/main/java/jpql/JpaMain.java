25ad228031f1907ae490cb1a961192126cba3e30
sb0321 <woltaryksb1@gmail.com>
2021-04-15 13:22:19 +0900

    JPQL 경로 표현식

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -16,16 +17,22 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.changeTeam(team);
             member1.setAge(20);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.changeTeam(team);
             member2.setAge(10);
 
             em.persist(member1);
             em.persist(member2);
+
 
             em.flush();
             em.clear();
@@ -34,14 +41,15 @@ public class JpaMain {
 //            String query = "select substring(m.username, 2, 3) from Member m";
 //            String query = "select locate('de', 'abcdegf') from Member m";
 //            String query = "select size(t.members) from Team t";
-            String query = "select group_concat(m.username) from Member m";
+//            String query = "select group_concat(m.username) from Member m";
+
+
+            String query = "select m.username from Team t join t.members m";
 
             List<String> result = em.createQuery(query, String.class).getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
-
+            System.out.println("result = " + result);
+            
 //            String query = "select nullif(m.username, '관리자') from Member m";
 //            List<String> result = em.createQuery(query, String.class).getResultList();
 //
