78537ddc40b110f79e73e51c6d177f3fcc8e2182
sb0321 <woltaryksb1@gmail.com>
2021-04-15 09:22:27 +0900

    JPQL 연습 프로젝트 init

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,15 +1,9 @@
-package jpabook.jpashop;
-
-import jpabook.jpashop.domain.Member;
+package jpql;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.List;
 
 public class JpaMain {
 
@@ -24,7 +18,9 @@ public class JpaMain {
 
         try {
 
-
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
